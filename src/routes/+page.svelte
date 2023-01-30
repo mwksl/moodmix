0130328e866915182a3cec12a8c34d78f0d75f46
Matthew Stingel <matthew@gofranchisely.com>
2023-01-29 21:06:25 -0700

    feat: improve style on mobile devices

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { onMount } from 'svelte';
 
 	import TypeAhead from '$lib/TypeAhead.svelte';
-	import Recommendations from "$lib/Recommendations.svelte";
+	import Recommendations from '$lib/Recommendations.svelte';
 
 	let albumToRequest = {};
 	let requestCounter = 0;
@@ -23,7 +23,6 @@
 
 	let requestId = uuid();
 
-
 	function handleAlbumSelection(event) {
 		albumToRequest = event.detail;
 	}
@@ -38,7 +37,7 @@
 		}
 		// Set up an interval to check for a response every second
 		intervalId = setInterval(async () => {
-		// Increment the counter
+			// Increment the counter
 			requestCounter += 1;
 			const { data, error } = await supabase
 				.from('recommendations')
@@ -58,11 +57,14 @@
 	});
 </script>
 
-<div class="flex justify-center items-center">
-	<div class="w-3/6 mt-4 rounded-sm p-8 bg-slate-100">
+<div class="flex flex-col lg:flex-row justify-center items-center">
+	<div class="w-full lg:w-1/2 mt-4 lg:mt-0 rounded-sm p-8 bg-slate-100">
 		<article class="prose">
 			<h1>Moodmix</h1>
-			<p>Moodmix is an AI media recommender using ChatGPT. It suggests films, music, and movies based on the album you're playing. </p>
+			<p>
+				Moodmix is an AI media recommender using ChatGPT. It suggests films, music, and movies based
+				on the album you're playing.
+			</p>
 			<p>Note: It may take up to 60 seconds for the AI model to complete your request.</p>
 		</article>
 		<TypeAhead class="mt-2" on:albumSelected={handleAlbumSelection} />
@@ -89,10 +91,9 @@
 			<button
 				class="mt-4 bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4 rounded"
 				disabled={!albumToRequest.album || !albumToRequest.artist}
-				>
+			>
 				Submit
 			</button>
 		</form>
 	</div>
 </div>
-
