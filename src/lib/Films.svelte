c84683e018b7f4854aca0f19b66eeeedcf998997
Matthew Stingel <matthew@gofranchisely.com>
2023-02-13 16:59:47 -0700

    fix: remove undefined flash from data load

--- src/lib/Films.svelte
+++ src/lib/Films.svelte
@@ -11,14 +11,16 @@
 	});
 </script>
 
-{#each films as film}
-	<div class="card lg:card-side bg-base-100 shadow-xl">
-		<figure><img src={film.posterPathLarge} alt="Movie Poster" /></figure>
-		<div class="card-body">
-			<h2 class="card-title">{film?.original_title}</h2>
-			<h3>{film?.director}</h3>
-			<p>{film?.overview}</p>
-			<div class="card-actions justify-end" />
+{#if films.length > 0}
+	{#each films as film}
+		<div class="card lg:card-side bg-base-100 shadow-xl">
+			<figure><img src={film.posterPathLarge} alt="Movie Poster" /></figure>
+			<div class="card-body">
+				<h2 class="card-title">{film?.original_title}</h2>
+				<h3>{film?.director}</h3>
+				<p>{film?.overview}</p>
+				<div class="card-actions justify-end" />
+			</div>
 		</div>
-	</div>
-{/each}
+	{/each}
+{/if}
