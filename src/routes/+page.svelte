68eb924d815dd784d7798580b70672f82bee4420
mwksl <matthewstingel@fastmail.com>
2024-11-28 12:17:38 -0700

    kill it

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,21 +27,33 @@
     src: url('https://dl.dropboxusercontent.com/s/g8xwlvvxhc4s0dg/ChicagoFLF.ttf') format('truetype');
   }
 
+  :global(body) {
+    margin: 0;
+    padding: 0;
+    height: 100vh;
+    background-color: #fff;
+  }
+
   .mac-error-screen {
     display: flex;
     justify-content: center;
     align-items: center;
     min-height: 100vh;
+    width: 100%;
     background-color: #fff;
     font-family: 'ChicagoFLF', monospace;
+    padding: 20px;
+    box-sizing: border-box;
   }
 
   .error-box {
     background-color: #fff;
     border: 3px solid #000;
-    width: 512px;
+    width: min(512px, 100%);
     padding: 16px;
     box-shadow: 3px 3px 0 #000;
+    position: relative;
+    box-sizing: border-box;
   }
 
   .error-content {
@@ -53,7 +65,7 @@
   }
 
   .bomb {
-    font-size: 64px;
+    font-size: min(64px, 15vw);
     line-height: 1;
   }
 
@@ -63,13 +75,13 @@
   }
 
   .error-text {
-    font-size: 18px;
+    font-size: min(18px, 5vw);
     margin-bottom: 16px;
     padding: 0 20px;
   }
 
   .error-details {
-    font-size: 14px;
+    font-size: min(14px, 4vw);
     margin: 0 auto;
     max-width: 400px;
     line-height: 1.4;
